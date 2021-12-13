cc580b4971530d454c425252b2f838bc4d25b72e
JeanClem <tovolahyjeanclement@gmail.com>
2021-12-13 07:08:52 +0300

    Update product-description.component.ts

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'app/product.service';
 import { Album } from 'app/album';
-import {ProductService } from ../product.service
+import {ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-description',
